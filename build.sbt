6312b8f193e86d3a5f54296d7ad93770d15e959e
minhthai <phammt@garena.com>
2018-07-10 18:26:21 +0800

    Update

--- build.sbt
+++ build.sbt
@@ -2,11 +2,17 @@ val commonSettings = Seq(
   version := "1.0.0",
   scalaVersion := "2.11.12",
 
+  resolvers ++= Seq(
+    Resolver.sonatypeRepo("releases"),
+    Resolver.sonatypeRepo("snapshots")
+  ),
+
   libraryDependencies ++= Seq(
     "org.apache.spark" %% "spark-core" % "2.1.0",
     "org.apache.spark" %% "spark-sql" % "2.1.0",
 
     // other useful libraries
+    "com.chuusai" %% "shapeless" % "2.3.3",
     "com.kailuowang" %% "henkan-convert" % "0.6.2",
 
     // for testing
@@ -15,31 +21,12 @@ val commonSettings = Seq(
     "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % "1.1.6" % Test
   ),
 
-  // fork in run := true,
-
-  // assemblyMergeStrategy in assembly := {
-  //   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
-
-  //   case x: Any =>
-  //     val oldStrategy = (assemblyMergeStrategy in assembly).value
-  //     oldStrategy(x)
-  // },
-
-  // disable test when package
-  // test in assembly := {},
+  // Show more concise errors
+  // https://github.com/tek/splain
+  addCompilerPlugin("io.tryp" % "splain" % "0.3.1" cross CrossVersion.patch),
 
   scalacOptions := Seq("-unchecked", "-deprecation"),
-
-  // Spark depends on an ancient version of Shapeless, shade it so a newer version
-  // can be used
-  // assemblyShadeRules in assembly := Seq(
-  //   ShadeRule.rename("shapeless.**" -> "shadeshapless.@1").inAll
-  // )
 )
 
 val generic_spark = (project in file("generic_spark"))
   .settings(commonSettings)
-  // .settings(
-  //   mainClass in assembly := Some(s"generic_spark.Main"),
-  //   assemblyJarName in assembly := s"generic_spark.jar"
-  // )
--- generic_spark/src/main/scala/Main.scala
+++ generic_spark/src/main/scala/Main.scala
@@ -1,8 +1,11 @@
 package generic_spark
 
-import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.{Dataset, Encoder}
 import shapeless._
+import shapeless.poly._
 import utils._
+
+import scala.reflect.runtime.universe._
 
 /**
   * Scenario:
@@ -19,7 +22,9 @@ trait HasAmount {
 }
 
 case class User(name: String)
+
 case class Txn(user_name: String, amount: Int) extends HasAmount
+
 case class Revenue(user_name: String, revenue: Int)
 
 object Main extends SparkLocal {
@@ -36,55 +41,56 @@ object Main extends SparkLocal {
 
     getUserRevenue(users, txns).show()
 
-    getUserRevenue2(users, txns).show()
-
     spark.stop()
   }
 
-  object getUserRevenue2 {
+  object getUserRevenue {
+
     object getKey extends Poly1 {
-      implicit val userCase: Case.Aux[User, String] = at((u: User) => u.name)
-      implicit val txnCase: Case.Aux[Txn, String] = at((t: Txn) => t.user_name)
+      implicit val caseUser: Case.Aux[User, Tuple1[String]] =
+        at(u => Tuple1(u.name))
+      implicit val caseTxn: Case.Aux[Txn, Tuple1[String]] =
+        at(t => Tuple1(t.user_name))
     }
 
-    def apply(users: Dataset[User], txns: Dataset[Txn]): Dataset[Revenue] =
+    def apply(users: Dataset[User], txns: Dataset[Txn]): Dataset[Revenue] = {
       getUserRevenueGeneric(users, txns, getKey) { (user, revenue) =>
         Revenue(user.name, revenue)
       }
+    }
   }
 
-  def getUserRevenueGeneric[U, T <: HasAmount, Out]
-  (users: Dataset[U], txns: Dataset[T], getKey: Poly)
-  (output: (U, Int) => Out)
-  : Dataset[Out] = {
+  def getUserRevenueGeneric[U <: Product, T <: Product with HasAmount, K <: Product, Out <: Product](
+    users: Dataset[U],
+    txns: Dataset[T],
+    getKey: Poly1
+  )(
+    output: (U, Int) => Out
+  )(implicit
+    // requires that `getKey` can handle `U` and `T`
+    caseU: Case.Aux[getKey.type, U :: HNil, K],
+    caseT: Case.Aux[getKey.type, T :: HNil, K],
+    // requirements from Spark API
+    e0: Encoder[K],
+    t0: TypeTag[U],
+    t1: TypeTag[T],
+    t2: TypeTag[Out],
+    t3: TypeTag[K]
+  ): Dataset[Out] = {
+
     users
-      .leftJoin(txns)(u => getKey(u), t => getKey(t)) // returns Dataset[(U, Option[T])]
+//      .joinWith[T](txns, $"name" === $"user_name", "left") // Dataset[(U, T)]
+      .leftJoin(txns)(u => getKey(u), t => getKey(t)) // Dataset[(U, T)]
+      .groupByKey { case (u, _) => getKey(u) }
+      .flatMapGroups {
+        case (_, iter) =>
+          val group = iter.toSeq
+          val _users = group.map(_._1)
+          val maybeTxns = group.map(_._2)
 
-      .groupByKey { case (u, t) => getKey(u) }
-      .flatMapGroups { case (_, iter) =>
-        val group = iter.toSeq
-        val _users = group.map(_._1)
-        val maybeTxns = group.map(_._2)
+          val revenue = maybeTxns.flatten.map(_.amount).sum
 
-        val revenue = maybeTxns.flatten.map(_.amount).sum
-
-        Seq(output(_users.head, revenue))
-      }
-  }
-
-  def getUserRevenue(users: Dataset[User], txns: Dataset[Txn]): Dataset[Revenue] = {
-    users
-      .leftJoin(txns)(_.name, _.user_name) // returns Dataset[(User, Option[Txn])]
-
-      .groupByKey { case (u, t) => (u.name) }
-      .flatMapGroups { case (_, iter) =>
-        val group = iter.toSeq
-        val _users = group.map(_._1)
-        val maybeTxns = group.map(_._2)
-
-        val revenue = maybeTxns.flatten.map(_.amount).sum
-
-        Seq(Revenue(_users.head.name, revenue))
+          Seq(output(_users.head, revenue))
       }
   }
 }
